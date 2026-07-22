30299c5f06014d9c75573c2fd68a627905d2450c
Parmar <sugam.parmar@edublockpro.com>
2026-07-22 23:28:32 +0530

    schema validations

--- src/models/user.js
+++ src/models/user.js
@@ -2,23 +2,51 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true,
+        minLength: 4,
     },
     lastName: {
         type: String
     },
     emailId: {
-        type: String
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+
     },
     password: {
-        type: String
+        type: String,
+        required: true,
+
     },
     age: {
-        type: Number
+        type: Number,
+        min: 18,
     },
     gender: {
+        type: String,
+        validate(value) {
+            if (!["male", "female", "other"].includes(value)) {     
+                throw new Error("Gender must be either male, female, or other");
+            }
+        }
+    },
+    photoUrl: {
         type: String
-    }
+    },
+    about: {
+        type: String,
+        default: "This is a default about me description."
+    },
+    skills: {
+        type: [String]
+    },
+},
+{
+    timestamps : true
 });
 
 
