aa0e43ef6ff9396d2c5f54926b4e9771d0cf1bb3
Parmar <sugam.parmar@edublockpro.com>
2026-08-10 15:35:45 +0530

    Fix connectionRequest model: add missing reference to User collection for toUserId

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,47 +2,94 @@ const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequestModel = require("../models/connectionRequest");
 const userRouter = express.Router();
+const User = require("../models/user");
 
-const USER_SAFE_DATA = ["firstName", "lastName", "photoUrl", "age", "about", "gender", "skills"]; 
+const USER_SAFE_DATA = [
+  "firstName",
+  "lastName",
+  "photoUrl",
+  "age",
+  "about",
+  "gender",
+  "skills",
+];
 // Get all the pending connection requests for the logged in user
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
-    try {
-        const loggedInUser =  req.user; 
-        const connectionRequests = await ConnectionRequestModel.find({
-            toUserId: loggedInUser._id,
-            status: "interested"
-        }).populate("fromUserId", "firstName lastName photoUrl age about gender skills"); // Populate the fromUserId field with user details
-        //  }).populate("fromUserId", ["firstName", "lastName"]);
+  try {
+    const loggedInUser = req.user;
+    const connectionRequests = await ConnectionRequestModel.find({
+      toUserId: loggedInUser._id,
+      status: "interested",
+    }).populate(
+      "fromUserId",
+      "firstName lastName photoUrl age about gender skills",
+    ); // Populate the fromUserId field with user details
+    //  }).populate("fromUserId", ["firstName", "lastName"]);
 
-        res.json({
-            message: "Connection requests fetched successfully",
-            data: connectionRequests
-        })
-    }catch (err) {
-        res.status(400).send("ERROR: " + err.message);
-    }
-})
-
+    res.json({
+      message: "Connection requests fetched successfully",
+      data: connectionRequests,
+    });
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
+  }
+});
 
 userRouter.get("/user/connections", userAuth, async (req, res) => {
-    try {
-const loggedInUser =  req.user;
-const connectionRequests = await ConnectionRequestModel.find({
-    $or: [
+  try {
+    const loggedInUser = req.user;
+    const connectionRequests = await ConnectionRequestModel.find({
+      $or: [
         { fromUserId: loggedInUser._id, status: "accepted" },
-        { toUserId: loggedInUser._id, status: "accepted" }
-    ]
-}).populate("fromUserId", USER_SAFE_DATA)
-const data = connectionRequests.map(row => row.fromUserId);
-res.json({
-    data,
-    message: "Connections fetched successfully"
-})
+        { toUserId: loggedInUser._id, status: "accepted" },
+      ],
+    })
+      .populate("fromUserId", USER_SAFE_DATA)
+      .populate("toUserId", USER_SAFE_DATA);
 
+    const data = connectionRequests.map((row) => {
+      if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+        return row.toUserId;
+      }
+      return row.fromUserId;
+    });
+    res.json({
+      data,
+      message: "Connections fetched successfully",
+    });
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
+  }
+});
 
-}catch (err) {
-        res.status(400).send("ERROR: " + err.message);
-    }
-})
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    //  Find all connectionRequests (send + received) for the logged in user
+
+    const connectionRequests = await ConnectionRequestModel.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+
+    const hideUsersFromFeed = new Set();
+    connectionRequests.forEach((req) => {
+      hideUsersFromFeed.add(req.fromUserId.toString());
+      hideUsersFromFeed.add(req.toUserId.toString());
+    });
+
+    const users = await User.find({
+      $and: [
+        { _id: { $ne: loggedInUser._id } }, // Exclude the logged-in user
+        { _id: { $nin: Array.from(hideUsersFromFeed) } }, // Exclude users with connection requests
+      ],
+    }).select(USER_SAFE_DATA);
+
+    res.send({
+      users,
+    });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 
 module.exports = userRouter;
